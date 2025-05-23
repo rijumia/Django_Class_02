61fe07916af0fcc85798acd690bf199a09c8d2d0
rijumia <riju.mia2612@gmail.com>
2025-05-23 15:30:58 +0600

    contact us page add

--- myProject/myProject/urls.py
+++ myProject/myProject/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from myProject.views import homePage
+from myProject.views import homePage, contactPage
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home', homePage),
+    path('contact', contactPage),
 ]
--- myProject/myProject/views.py
+++ myProject/myProject/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render, redirect
 
 def homePage(request):
     return render(request, 'home.html')
+
+def contactPage(request):
+    return render(request, 'contact.html')
